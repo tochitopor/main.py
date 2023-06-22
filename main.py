bdb2d2aaa6837baa547d034513a6d766a1447714
IAGanja <abichaika@yandex.ru>
2023-06-22 15:23:28 +0300

    создал основные методы,грубо,без проверок,без смс

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import json
+
 from fastapi import FastAPI, status, Body, Form
 import uuid
 from fastapi.responses import JSONResponse, FileResponse
 from ipaddress import IPv4Address
 
-
 import ipaddress
+
+
 # # для IP4-адреса
 # ipAddr = ipaddress.IPv4Address(address)
 
@@ -13,7 +16,11 @@ import ipaddress
 class Device:
     def __init__(self, name, ip):
         self.name = name
-        self.ip = str(ipaddress.IPv4Address(ip))  #str(IPv4Address)
+        self.ip = str(ipaddress.IPv4Address(ip))  # str(IPv4Address)
+
+    def toString(self):
+        return "name " + self.name + ", ip " + self.ip
+
 
 # условная база данных - набор объектов класса Device
 machine = [Device("CCP1", "192.168.1.102"), Device("CCP2", "192.168.1.103"),
@@ -29,6 +36,7 @@ machine = [Device("CCP1", "192.168.1.102"), Device("CCP2", "192.168.1.103"),
            Device("SDBRA", "192.168.1.121"), Device("SDBRB", "192.168.1.122"),
            Device("FTI1", "192.168.1.123")]
 
+
 # для поиска устройства в списке machine
 def find_device(ip):
     for device in machine:
@@ -36,42 +44,43 @@ def find_device(ip):
             return device
     return None
 
+
 app = FastAPI()
 
 
-# При обращении к корню веб-приложения, то есть по пути "/",
-# оно будет отправлять в ответ файл index.html, то есть веб-страницу,
-# посредством которой пользователь сможет взаимодействовать с сервером:
 @app.get("/")
 def root():
     return FileResponse("public/index.html")
 
-# # Для обработки полученных в POST-запросе данных по адресу "/hello" определена функция hello().
-# # Эта функция имеет один параметр - data, который получает содержимое тела запроса:
-# @app.post("/hello")
-# def hello(data = Body()):
-#     name = data["name"]
-#     age = data["age"]
-#     return {"message": f"{name}, ваш возраст - {age}"}
-# # То есть здесь data будет представлять весь объект, который отправляется с веб-страницы и который
-# # имеет свойства "name" и "age". Этот объект в python будет представлять словарь. Соответственно, чтобы получить
-# # значения свойства "name", обращаемся по одноименному ключу: name = data["name"]
+
+@app.put("/update_device", status_code=status.HTTP_201_CREATED)
+def update_device(data=Body()):
+    # получаем IP адрес виртуального канала по id
+    device = find_device(data["vl_ip"])
+
+    # если не найдено, отправляем статусный код и сообщение об ошибке
+    if device == None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Упс, устройство не найдено"}
+        )
+
+    # если устройство найдено, удаляем его добавляем новое
+    machine.remove(device)
+    updateDevice = Device(data["vl_name"], data["vl_ip"])
+    machine.append(updateDevice)
+    return updateDevice
+
+@app.post("/add_device", status_code=status.HTTP_201_CREATED)
+def add_device(data=Body()):
+    postDevice = Device(data["vl_name"], data["vl_ip"])
+    machine.append(postDevice)
+    return postDevice
 
 
-# В функции add_vl, которая обрабатывает запрос по одноименному пути, через параметры получаем
-# отправленные данные. Причем параметры называются также, как и атрибуты name у полей формы.
-# А самим параметрам присваивается объект Form.
-@app.post("/add_vl", status_code=status.HTTP_201_CREATED)
-def add_vl(vl_name=Form(), vl_ip=Form()):
-    return {"vl_name": vl_name, "vl_ip": vl_ip}
-
-# status_code является атрибутом метода-декоратора (get, post и т.д.),
-# а не функции-обработчика пути в отличие от всех остальных параметров и тела запроса.
-
-
-# кнопка удаления последней строки таблицы - Delete VL
-@app.post("/delete_vl", status_code=status.HTTP_200_OK)
-def delete_last_vl(ip):
+# кнопка удаления по ip
+@app.delete("/delete_device/{ip}", status_code=status.HTTP_200_OK)
+def delete_device(ip):
     # получаем IP адрес виртуального канала по id
     device = find_device(ip)
 
@@ -87,19 +96,10 @@ def delete_last_vl(ip):
     return device
 
 
-
-# Извлечь переменные пути из маршрута, определим их при объявлении маршрута, а затем передадим их в функцию маршрута.
-@app.get("/all_list/{ip_vl}")
-async def ip_ping(ip_vl: str):
-    return {"ip_vl":ip_vl}
-
-
-
 @app.get("/device/{ip}")
 def get_device(ip):
     # получаем устройство по ip
     device = find_device(ip)
-    print(device)
     # если устройство не найдено, отправляем статусный код и сообщение об ошибке
     if device == None:
         return JSONResponse(
@@ -108,3 +108,7 @@ def get_device(ip):
         )
     # если устройство найдено, отправляем его
     return device
+
+@app.get("/alldevices")
+def get_alldevices():
+    return machine
